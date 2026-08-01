567e6835fdc5e51cac0f4db05650a7722e03611f
techplanshetyapps <rudnikova.nataliia@gmail.com>
2026-08-01 07:57:34 +0000

    Add statements

--- tSQL.sql
+++ tSQL.sql
@@ -1,3 +1,22 @@
 -- regular expressions
 -- create a sequence
-CREATE SEQUENCE START INCREMENT MINVALUE MAXVALUE
+CREATE SEQUENCE SchemaName.SequenceName
+    AS INT
+    START WITH 1
+    INCREMENT BY 1
+    MINVALUE 1
+    MAXVALUE 1000;
+GO
+
+-- create a table
+CREATE TABLE SchemaName.TableName (
+    IDColumn INT DEFAULT (NEXT VALUE FOR SchemaName.SequenceName),
+    ReferenceID INT,
+    ColumnName VARCHAR(100),
+    FilterColumn INT,
+    DataColumn VARCHAR(100)
+);
+GO
+
+-- sequence values 
+UPDATE SET
